0cb1d53403f9248df22906b0e10a9b5658ca2f8e
SreenathKannadasan <sreenath.kannadasan@serole.com>
2025-10-16 14:04:39 +0530

    Delete enterprise directory

--- enterprise/iot/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import test_printer_tour
-from . import test_ingenico_driver
--- enterprise/point_of_sale/static/tests/unit/data/pos_printer.data.js
+++
@@ -1,9 +0,0 @@
-import { models } from "@web/../tests/web_test_helpers";
-
-export class PosPrinter extends models.ServerModel {
-    _name = "pos.printer";
-
-    _load_pos_data_fields() {
-        return ["id", "name", "proxy_ip", "product_categories_ids", "printer_type"];
-    }
-}
